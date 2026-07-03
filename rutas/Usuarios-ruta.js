ca62d6b51975d4ece7b4d14336f52feb92640ad2
santiagoquintana574-collab <santiagoquintana574@gmail.com>
2026-07-03 15:55:19 +0000

    feat: Optimizando el llamado de los controles en el archivo Usuario-ruta.js

--- rutas/Usuarios-ruta.js
+++ rutas/Usuarios-ruta.js
@@ -5,15 +5,27 @@ const {VerUsuario, VerUsuarios, CrearUsuario,ActualizarUsuario, BorrarUsuario} =
 const router = express.Router();
 
 /////////////////////////////// Tabla Usuarios //////////////////////////////////////////// 
-router.get("/usuarios", VerUsuarios);
+// router.get("/usuarios", VerUsuarios);
 
-router.get("/usuarios/:id", VerUsuario);
+// router.get("/usuarios/:id", VerUsuario);
 
-router.post("/usuarios", CrearUsuario);
+// router.post("/usuarios", CrearUsuario);
 
-router.patch("/usuarios/:id", ActualizarUsuario); // la diferencia entre patch y put es que patch actualiza parcialmente no completo como put
+// router.patch("/usuarios/:id", ActualizarUsuario); // la diferencia entre patch y put es que patch actualiza parcialmente no completo como put
 
-router.delete("/usuarios/:id", BorrarUsuario);
+// router.delete("/usuarios/:id", BorrarUsuario);
+
+
+///////// forma optima ////////////////////////////////////////////////////////////////////
+
+router.route("/usuarios")
+    .get(VerUsuarios)
+    .post(CrearUsuario);
+
+router.route("/usuarios/:id")
+    .get(VerUsuario)
+    .patch(ActualizarUsuario)
+    .delete(BorrarUsuario);
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
