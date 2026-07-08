fc2bc0828ea34036d8e4d921563f95b79451c7e3
santiagoquintana574-collab <santiagoquintana574@gmail.com>
2026-07-08 12:27:50 +0000

    Feat : configurando la base de datos

--- controles/Usuario-controles.js
+++ controles/Usuario-controles.js
@@ -6,7 +6,7 @@ const VerUsuarios = async (req, res) => {
     try {
         const usuarios = await usuariosService.obtenerTodos()
         res.json(usuarios);
-        console.log(usuarios);
+        // console.log(usuarios);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Algo salió mal en el servidor" });
--- db/db.js
+++ db/db.js
@@ -3,11 +3,11 @@ const {config} = require("dotenv");
 config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    host: "localhost",
+    user: "postgres",
+    password: "1234567890qwertyuiop",
+    database: "planetaestacionamiento",
+    port: "5432",
 });
 
 pool.connect()
