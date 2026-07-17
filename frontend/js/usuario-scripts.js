a75e9e981e4fe380dbaf17da19bf187f498b72f8
llonigro <llonigro@fi.uba.ar>
2026-07-17 20:30:28 -0300

    feat: agregar campos a 'nosotros' y placeholders a 'inicio'

--- frontend/js/usuario-scripts.js
+++ frontend/js/usuario-scripts.js
@@ -1,8 +1,11 @@
 async function registrarUsuario() {
-    const usuario = document.getElementById("usuario").value;
+    // Asignar los valores de los campos del formulario a variables
+    try {
+    const usuario = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+    // Hacer la solicitud POST al backend para registrar el usuario
     const respuesta = await fetch('http://localhost:3000/usuarios', {
         method: 'POST',
         headers: {
@@ -11,11 +14,19 @@ async function registrarUsuario() {
         body: JSON.stringify({ usuario, email, password })
     });
 
+    // Manejar la respuesta del backend
+    if (!respuesta.ok) {
+        throw new Error('Error al registrar usuario');
+
+    }
+
+    // En caso de éxito, mostrar un mensaje de éxito 
     const data = await respuesta.json();
-    if (respuesta.status === 201) {
-        alert('Usuario registrado exitosamente');
-    } else {
-        alert('Error al registrar usuario: ' + data.error);
+    console.log('Usuario registrado con éxito:', data);
+
+    // En caso de error, mostrar un mensaje de error  
+    } catch (error) {
+    console.error('Error al registrar usuario:', error);
     }
 }
 
