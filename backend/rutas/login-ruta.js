a9598abd9851065f395d96981b91c69e5046c19e
santiagoquintana574-collab <santiagoquintana574@gmail.com>
2026-08-05 22:42:43 +0000

    fix: Resolviendo conflictos de merge

--- backend/rutas/login-ruta.js
+++ backend/rutas/login-ruta.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const { login, logout } = require("../controles/login-controles.js");
 const { validarLogin } = require("../validacion/validacion-login.js");
-=======
-const { login } = require('../controles/login-controles.js'); // agregar a logout si es necesario
-const { validarLogin } = require('../validacion/validacion-login.js');
->>>>>>> 7533cd532b774ba17e4283ff0caebb583882a1da
 
 // Endpoint: POST
 router.post("/login", validarLogin, login);
 
-<<<<<<< HEAD
 router.post("/logout", logout);
 
 module.exports = router;
-=======
-// router.post('/logout', logout);
-
-module.exports = router;
->>>>>>> 7533cd532b774ba17e4283ff0caebb583882a1da
